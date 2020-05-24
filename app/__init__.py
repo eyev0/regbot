5f66eb4370b9aee91f4825cd4c239d1719fe3904
Egor Dementyev <bespamiatny@gmail.com>
2020-05-24 20:54:02 +0300

    proxy cmdline arg

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 import pytz
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.files import JSONStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 from app.config import Config, APP_NAME
@@ -18,8 +17,15 @@ parser.add_argument('-t', '--test', dest='test_env',
                     action='store_true', default=False)
 parser.add_argument('-c', '--container', dest='container',
                     action='store_true', default=False)
+parser.add_argument('-w', '--webhook', dest='webhook_mode',
+                    action='store_true', default=False)
+parser.add_argument('-p', '--proxy', dest='proxy',
+                    action='store_true', default=False)
 args = parser.parse_args()
-config = Config(args.container, args.test_env)
+config = Config(args.container,
+                args.test_env,
+                args.webhook_mode,
+                args.proxy)
 
 file_handler = logging.FileHandler(config.log_path)
 stdout_handler = logging.StreamHandler(sys.stderr)
@@ -28,7 +34,7 @@ logging.basicConfig(format=u'%(filename)s [ LINE:%(lineno)+3s ]#%(levelname)+8s 
                     level=logging.INFO,
                     handlers=(file_handler, stdout_handler,))
 
-bot = Bot(token=config.TOKEN, proxy=config.PROXY_URL)
+bot = Bot(token=config.TOKEN, proxy=config.PROXY_URL, proxy_auth=config.PROXY_AUTH)
 dp = Dispatcher(bot, storage=config.states_storage)
 dp.middleware.setup(LoggingMiddleware())
 
--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import json
 
+import aiohttp
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 APP_NAME = 'regbot'
@@ -7,17 +8,19 @@ APP_NAME = 'regbot'
 
 class Config(object):
 
-    def __init__(self, container, test_env):
+    def __init__(self, container, test_env, webhook_mode, proxy):
         self.container = container
         self.test_env = test_env
+        self.webhook_mode = webhook_mode
+        self.use_proxy = proxy
 
         if self.container:
             self.vol_path = '/vol'
         else:
             self.vol_path = f'/home/egor/{APP_NAME}'
 
-        self.config_path = self.vol_path + '/config.json'
-        self.proxy_path = self.vol_path + '/proxy.json'
+        self.config_json = self.vol_path + '/config.json'
+        self.proxy_json = self.vol_path + '/proxy.json'
         self.log_path = self.vol_path + f'/{APP_NAME}{"-test" if test_env else ""}.log'
 
         self.db_dialect = 'postgres'
@@ -54,18 +57,29 @@ class Config(object):
                                                   db=0,
                                                   prefix=prefix + 'notify')
 
-        with open(self.config_path, 'rb') as f:
+        with open(self.config_json, 'rb') as f:
             self.conf = json.load(f)
             self.TOKEN = self.conf['TOKEN' if not test_env else 'TOKEN_TEST']
             self.check_admin = self.conf['check_admin']
             self.admins = []
 
-        with open(self.proxy_path, 'rb') as f:
-            self.proxy_conf = json.load(f)
-            self.PROXY_PROTOCOL = self.proxy_conf['protocol']
-            self.PROXY_IP = self.proxy_conf['ip']
-            self.PROXY_PORT = self.proxy_conf['port']
-            self.PROXY_URL = self.PROXY_PROTOCOL + '://' + self.PROXY_IP + ':' + self.PROXY_PORT
+        if self.use_proxy:
+            with open(self.proxy_json, 'rb') as f:
+                self.proxy_conf = json.load(f)
+                self.PROXY_PROTOCOL = self.proxy_conf['protocol']
+                self.PROXY_IP = self.proxy_conf['ip']
+                self.PROXY_PORT = self.proxy_conf['port']
+                self.PROXY_USERNAME = self.proxy_conf['username'] or ''
+                self.PROXY_PASSWD = self.proxy_conf['password'] or ''
+                self.PROXY_URL = self.PROXY_PROTOCOL + '://' + self.PROXY_IP + ':' + self.PROXY_PORT
+
+                if len(self.PROXY_USERNAME) > 0:
+                    self.PROXY_AUTH = aiohttp.BasicAuth(login=self.PROXY_USERNAME, password=self.PROXY_PASSWD)
+                else:
+                    self.PROXY_AUTH = None
+        else:
+            self.PROXY_URL = None
+            self.PROXY_AUTH = None
 
     def __repr__(self):
         return f'Config(container={self.container}, test_env={self.test_env}, TOKEN={self.TOKEN}, ' \
